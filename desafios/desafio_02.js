1c958b04fe2a7a11dd84774a8f3a859fa97d98d3
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-23 21:15:40 -0500

    ✨ add desafio_02 with various user interaction prompts

---
+++ desafios/desafio_02.js
@@ -0,0 +1,37 @@
+// # 01
+
+diaSemana = prompt("Qué día de la semana es?");
+if (diaSemana == "Sábado" || diaSemana == "Domingo") {
+    alert("¡Buen fin de semana!")
+} else {
+    alert("¡Buena semana!")
+}
+
+// # 02
+
+num = prompt("Ingrese un número");
+if (num >= 0) {
+    alert("El número es positivo")
+} else {
+    alert("El número es negativo")
+}
+
+// # 03
+
+score = prompt("Ingrese su puntaje");
+if (score >= 100) {
+    alert("¡Felicidades, has ganado!")
+} else {
+    alert("Intentalo nuevamente para ganar.")
+}
+
+// # 04
+
+saldo = 1000
+user = "David Gilmour"
+alert(`Bienvenido(a) ${user}, tu saldo es de ${saldo}`)
+
+// # 05
+
+nombre = prompt("Por favor, ingresá tu nombre")
+alert(`¡Hola ${nombre}!`)
