88c956e742cc1a15775a828c294f4b16c27ae366
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-23 21:17:13 -0500

    ✨ improve user prompts and add missing semicolons in desafio_02.js

--- desafios/desafio_02.js
+++ desafios/desafio_02.js
@@ -2,36 +2,36 @@
 
 diaSemana = prompt("Qué día de la semana es?");
 if (diaSemana == "Sábado" || diaSemana == "Domingo") {
-    alert("¡Buen fin de semana!")
+    alert("¡Buen fin de semana!");
 } else {
-    alert("¡Buena semana!")
+    alert("¡Buena semana!");
 }
 
 // # 02
 
 num = prompt("Ingrese un número");
 if (num >= 0) {
-    alert("El número es positivo")
+    alert("El número es positivo");
 } else {
-    alert("El número es negativo")
+    alert("El número es negativo");
 }
 
 // # 03
 
 score = prompt("Ingrese su puntaje");
 if (score >= 100) {
-    alert("¡Felicidades, has ganado!")
+    alert("¡Felicidades, has ganado!");
 } else {
-    alert("Intentalo nuevamente para ganar.")
+    alert("Intentalo nuevamente para ganar.");
 }
 
 // # 04
 
-saldo = 1000
-user = "David Gilmour"
-alert(`Bienvenido(a) ${user}, tu saldo es de ${saldo}`)
+saldo = 1000;
+user = "David Gilmour";
+alert(`Bienvenido(a) ${user}, tu saldo es de ${saldo}`);
 
 // # 05
 
-nombre = prompt("Por favor, ingresá tu nombre")
-alert(`¡Hola ${nombre}!`)
+nombre = prompt("Por favor, ingresá tu nombre");
+alert(`¡Hola ${nombre}!`);
